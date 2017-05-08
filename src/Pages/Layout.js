fc865fa3fc6151a10758459855319e40734f6a02
rhenderson07 <rhenderson07@gmail.com>
2017-05-08 09:50:35 -0400

    Move Header Components inside Layout Component

--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
-import Header from './Layout/Header';
+import logo from './logo.svg';
 
 class Layout extends Component {
+
+  // Create presentation Component. Avoid needlessly creating files for small components
+  Header = () => {
+    return (
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h2>Welcome to React2</h2>
+      </div>
+    );
+  }
 
   render() {
     return (
       <div className="App">
-        <Header/>
+        <this.Header/>
         <div className="app-body">
           {this.props.children}
         </div>
